ef2f8b2fe795f28e6a9737362eab76c4f925594e
wei <weix13@ziroom.com>
2021-09-22 21:32:49 +0800

    feat:组件模版

---
+++ template.vue
@@ -0,0 +1,16 @@
+<template>
+    <view class="map-container">
+        
+    </view>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({name:'piMap'}) 
+export default class piMap extends Vue{
+
+}
+</script>
+<style lang="scss">
+
+</style>
